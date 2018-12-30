a75a3c2448b92f3515d4cda994476c6809338f07
biqiaoran629 <biqiaoran@hotmail.com>
2018-12-30 15:51:26 -0800

    TextFieldGroup component

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div>
       <footer className="bg-dark text-white mt-5 p-4 text-center">
-        Copyright &copy; {new Date().getFullYear} Dev Connector
+        Copyright &copy; {new Date().getFullYear()} Dev Connector
       </footer>
     </div>
   );
