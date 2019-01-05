882d7e96fc9d8c81b2cf02a248ac3025f84e3627
biqiaoran629 <biqiaoran@hotmail.com>
2019-01-04 20:45:20 -0800

    Private route

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -11,27 +11,28 @@ class Dashboard extends Component {
   }
 
   render() {
-
     const { user } = this.props.auth;
     const { profile, loading } = this.props.profile;
 
     let dashboardContent;
 
     if (profile === null || loading) {
-      dashboardContent = <Spinner />
+      dashboardContent = <Spinner />;
     } else {
       // Check if logged in user has profile data
       if (Object.keys(profile).length > 0) {
-        dashboardContent = <h4>Display Profile</h4>
+        dashboardContent = <h4>Display Profile</h4>;
       } else {
         // User is logged in but has no profile
         dashboardContent = (
           <div>
             <p className="lead text-muted">Welcome {user.name}</p>
             <p>You have not yet setup a profile, please add some info</p>
-            <Link to="/create-profile" className="btn btn-lg btn-info">Create Profile</Link>
+            <Link to="/create-profile" className="btn btn-lg btn-info">
+              Create Profile
+            </Link>
           </div>
-        )
+        );
       }
     }
 
@@ -59,7 +60,7 @@ Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   profile: PropTypes.object.isRequired
-}
+};
 
 export default connect(
   mapStateToProps,
