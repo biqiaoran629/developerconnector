66c1b2fd95f3a17a4ec928cdb3d03dc08c564c64
biqiaoran629 <biqiaoran@hotmail.com>
2019-01-19 23:38:34 -0800

    profile sub components

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ import EditProfile from "./components/edit-profile/EditProfile";
 import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
 import store from "./store";
 
 import "./App.css";
@@ -53,6 +54,8 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:handle" component={Profile} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
@@ -84,7 +87,6 @@ class App extends Component {
                   component={AddEducation}
                 />
               </Switch>
-              <Route exact path="/profiles" component={Profiles} />
             </div>
             <Footer />
           </div>
--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -23,6 +23,20 @@ export const getCurrentProfile = () => dispatch => {
     .catch(err => dispatch({ type: GET_PROFILE, payload: {} }));
 };
 
+// Get profile by handle
+export const getProfileByHandle = handle => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get(`/api/profile/handle/${handle}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err => dispatch({ type: GET_PROFILE, payload: {} }));
+};
+
 // Create profile
 export const createProfile = (profileData, history) => dispatch => {
   axios
